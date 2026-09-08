c907fcc660221374076ad91cb9a22f5e4ff78dd8
Syam Alamgir <96352445+seeyum@users.noreply.github.com>
2026-09-07 20:11:42 -0400

    Delete src directory

--- src/pages/PrivacyPolicy.tsx
+++
@@ -1,7 +0,0 @@
-import { useLanguage } from "../i18n/LanguageContext";
-import LegalPage from "./LegalPage";
-
-export default function PrivacyPolicy() {
-  const { t } = useLanguage();
-  return <LegalPage title={t.legal.privacy.title} sections={t.legal.privacy.sections} current="privacy" />;
-}
--- src/pages/TermsAndConditions.tsx
+++
@@ -1,7 +0,0 @@
-import { useLanguage } from "../i18n/LanguageContext";
-import LegalPage from "./LegalPage";
-
-export default function TermsAndConditions() {
-  const { t } = useLanguage();
-  return <LegalPage title={t.legal.terms.title} sections={t.legal.terms.sections} current="terms" />;
-}
